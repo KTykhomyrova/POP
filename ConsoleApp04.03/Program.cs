dcd1d2a59ff167b5fc848348af94b301c646afc6
KTykhomyrova <tykhomyrovak@gmail.com>
2025-03-04 09:32:18 +0100

    Add files via upload

---
+++ ConsoleApp04.03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp04._03 {
+	internal class Program {
+
+		class Employee {
+			//brak inkapsulacji, niepoprawne użycie pól zamiast właściwości (nie mamy kontroli nad ustawianiem wartości)
+			//brak zabiezpieczenia pzed pustym imieniem
+			public string Name;
+			public int Age; //błędnr typ dannych(powinniśmy zastosować typ: bajt (0-255), ushort(0-65535), DataTime(misimy dodać metodę obliczająca wiek w czasie rzeczywistym))
+			public double Salary;
+			
+			public Employee(string name, int age, double salary) {
+				Name = name;
+				Age = age;
+				Salary = salary;
+			}
+
+			//brak walidacji w metodzie, nie sprawdzamy czy amount jest dodatnie
+			public void IncreaseSalary(double amount) {
+				Salary += amount;
+			}
+		}
+		static void Main(string[] args) {
+			
+			Employee person = new Employee("Jan", 20, 1000.50);
+			person.Age = -5; // możemy ustawić błędny wiek
+			person.Salary = -1000; //możemy ustawić negatywną pensje
+
+			Console.WriteLine($"Pracownik: {person.Name}, {person.Age}, {person.Salary}\n");
+
+			
+
+			
+
+		}
+	}
+}
---
+++ ConsoleApp1_poprawionaWersja_04.03/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApp1_poprawionaWersja_04._03 {
+	internal class Program {
+		class Employee {
+			public string Name { get; }
+			public DateOnly DateOfBirth { get; }
+			public int Age => CalculateAge();
+
+			public Employee(string name, DateOnly dateOfBirth) {
+				Name = name;
+				DateOfBirth = dateOfBirth;
+			}
+
+			private int CalculateAge() {
+				var today = DateOnly.FromDateTime(DateTime.Today);
+				int age = today.Year - DateOfBirth.Year;
+
+				if(DateOfBirth > today.AddYears(-age))
+					age--;
+				return age;
+			}
+		}
+
+		static void Main(string[] args) {
+			string name = GetValidName();
+			DateOnly dateOfBirth = GetValidateDateOfBirth();
+
+			Employee emp = new Employee(name, dateOfBirth);
+			Console.WriteLine($"Pracownik: {emp.Name}, wiek: {emp.Age}");
+		}
+
+		private static string GetValidName() {
+			string name;
+			const sbyte MinLength = 3;
+			const sbyte MaxLength = 50;
+
+			string[] errorMessage = new string[] {
+				"imię nie może być puste!" +
+				$"Imie muśi mieć co najmniej {MinLength} znaków" +
+				$"Imie muśi mieć maksyamlne {MinLength} znaków"
+			};
+			do {
+				Console.WriteLine("POdsj imię");
+				name = Console.ReadLine()?.Trim();
+
+				if(string.IsNullOrWhiteSpace(name)) {
+					Console.WriteLine(errorMessage[0]);
+				}else if(name.Length < ) {
+
+				}
+			}
+			while(true);
+
+			do {
+				Console.WriteLine("Podaj imię");
+				name = Console.ReadLine()?.Trim();
+
+				if(string.IsNullOrWhiteSpace(name)) {
+					Console.WriteLine("Błąd: imię nie może być puste");
+				}
+			}
+			while(string.IsNullOrWhiteSpace(name));
+
+			return name;
+		}
+
+		private static DateOnly GetValidateDateOfBirth() {
+			DateOnly dateOfBirth;
+			bool isValidDate = false;
+
+			do {
+				Console.WriteLine("Podaj datę urodzenia (rrrr-mm-dd):");
+				string input = Console.ReadLine();
+
+				if(DateOnly.TryParseExact(input, out dateOfBirth) && dateOfBirth <= DateOnly.FromDateTime(DateTime.Now)) {
+					isValidDate = true;
+				} else {
+					Console.WriteLine("Błąd: niepoprawny format daty.");
+				}
+
+			} while(!isValidDate);
+
+			return dateOfBirth;
+		}
+	}
+}
